0c7bc0255cf4b5047e3d2b5fbdca51091bc92f51
Willian Ferreira <willian.ferreira.gomes96@gmail.com>
2024-06-23 18:32:51 -0300

    Mysql Conectado

--- Servidor/server.js
+++ Servidor/server.js
@@ -1,18 +1,39 @@
 //1° Inicializar o Node: npm init -y
 //2º Instalar o framework Express: npm install express
+//3º instalar o driver do Mysql: npm install mysql2
 
-//3º Importar o framework Express no arquivo: server.js
+//Importar o framework Express no server.js
+//Importar o driver Mysql no server.js
 const express = require('express');
-//4º Criar uma instância do express
+const mysql = require('mysql2');
+
+//Criar uma instância do express
 const app = express();
 
-//5º Declarar o nome da pasta que aloca o "index.html"
+//Declarar o nome da pasta que aloca o "index.html"
+app.use(express.static('Projeto Web'));
     /*  A pasta do projeto em questão precisa esta dentro
     da mesma pasta do arquivo "server.js" com um arquivo "index.html" */
-app.use(express.static('Projeto Web'));
 
-//6º Definir a porta do localhost
-const port = 3000;
+//Configurar conexão com o Banco de dados
+const db = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'root',
+  database:'my_first_db'
+});
+
+//Conectar ao banco de dados
+db.connect(err =>{
+  if(err){
+    console.error('Erro ao conectar ao banco de dados:', err);
+  }else{
+    console.log('Conectado ao banco de dados MySQL.');
+  }
+});
+
+//Definir a porta do localhost
+const port = 3000; 
 app.listen(port, ()=> {
     console.log(`Servidor está rodando em http://localhost:${port}`)
   });
@@ -20,5 +41,5 @@ app.listen(port, ()=> {
     , ()=> {console.log(`Servidor está rodando em http://localhost:${port}`)} dentro dele é apenas uma "Perfumaria"...
     ...para mostrar no console, que o código funcionou. */
 
-//7º Ligar o Servidor no terminal: node server.js
-//8º Desligar o Servidor no terminal: Ctrl + C
+//Ligar o Servidor no terminal: node server.js
+//Desligar o Servidor no terminal: Ctrl + C
